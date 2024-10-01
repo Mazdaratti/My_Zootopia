e3455adce5d8de4ebe5c9d164e28779e9b69b45e
Mazdaratti <vasya.mazdaratti@googlemail.com>
2024-10-01 21:36:57 +0200

    Refactoring
    the function generating the string with animal info is now splitted into two functions: serialize_animal() and serialize_animals(), simplifying the code

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,57 +2,63 @@ import json
 
 
 def load_json(file_path):
-    """Load a JSON file and return the data."""
+    """Load and return the data from a JSON file."""
     with open(file_path, "r") as json_file:
         return json.load(json_file)
 
 
-def generate_animal_info(animals_data):
-    """Generate formatted information for each animal."""
-    animals_info_str = ""
-    for animal in animals_data:
-        # Use .get() to safely access keys
-        name = animal.get("name")
-        characteristics = animal.get("characteristics", {})
-        diet = characteristics.get("diet")
-        animal_type = characteristics.get("type")
-        locations = animal.get("locations", [])
-        animals_info_str += "<li class='cards__item'>"
-        if name:
-            animals_info_str += f"Name: {name}<br/>\n"
-        if diet:
-            animals_info_str += f"Diet: {diet}<br/>\n"
-        if locations:
-            animals_info_str += f"Location: {locations[0]}<br/>\n"
-        if animal_type:
-            animals_info_str += f"Type: {animal_type}<br/>\n"
+def serialize_animal(animal_obj):
+    """Generate formatted information for a single animal."""
+    name = animal_obj.get("name")
+    characteristics = animal_obj.get("characteristics", {})
+    diet = characteristics.get("diet")
+    animal_type = characteristics.get("type")
+    locations = animal_obj.get("locations", [])
 
-        animals_info_str += "</li>"
+    # Using a list to accumulate strings for better performance
+    animal_info_parts = ["<li class='cards__item'>"]
 
-    return animals_info_str
+    if name:
+        animal_info_parts.append(f"Name: {name}<br/>\n")
+    if diet:
+        animal_info_parts.append(f"Diet: {diet}<br/>\n")
+    if locations:
+        animal_info_parts.append(f"Location: {locations[0]}<br/>\n")
+    if animal_type:
+        animal_info_parts.append(f"Type: {animal_type}<br/>\n")
+
+    animal_info_parts.append("</li>")
+
+    return ''.join(animal_info_parts)
+
+
+def serialize_animals(data):
+    """Serialize a list of animals data into an HTML string."""
+    return ''.join(serialize_animal(animal) for animal in data)
 
 
 def read_file(file_path):
-    """Read an HTML file and return its contents."""
-    with open(file_path, "r") as html_file:
-        return html_file.read()
+    """Read and return the contents of a file."""
+    with open(file_path, "r") as file:
+        return file.read()
 
 
 def write_file(file_path, content):
-    """Write the modified HTML content to a new file."""
-    with open(file_path, "w") as html_output:
-        html_output.write(content)
+    """Write the given content to a file."""
+    with open(file_path, "w") as file:
+        file.write(content)
 
 
 def main():
-    """Main function to generate animal HTML from the template."""
-    animal_data = load_json("animals_data.json")
-    animal_info = generate_animal_info(animal_data)
+    """Generate animal information and update the HTML template."""
+    data = load_json("animals_data.json")
+    animals_info = serialize_animals(data)
     template = read_file("animals_template.html")
-    updated_template = template.replace("__REPLACE_ANIMALS_INFO__", animal_info)
+    updated_template = template.replace("__REPLACE_ANIMALS_INFO__", animals_info)
     write_file("animals.html", updated_template)
 
 
 if __name__ == "__main__":
     main()
+
 
